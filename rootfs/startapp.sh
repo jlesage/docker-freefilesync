02a69dd14e5490ff27cb109f0483c7b3f5c5ff0e
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-05-12 16:45:40 -0400

    Removed unneeded variable export.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -3,8 +3,5 @@
 # Needed for Firefox.
 export HOME=/config
 
-# For GTK3 freeze : https://freefilesync.org/forum/viewtopic.php?t=10103
-export GDK_SYNCHRONIZE=1
-
 cd /storage
 exec /opt/FreeFileSync/Bin/FreeFileSync
