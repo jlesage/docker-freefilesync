b2b1ffaf131f662727bb6125bc953c311432cc30
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-05-14 18:19:45 -0400

    Start FreeFileSync with the last run profile.

--- rootfs/etc/cont-init.d/55-freefilesync.sh
+++ rootfs/etc/cont-init.d/55-freefilesync.sh
@@ -6,10 +6,12 @@ set -u # Treat unset variables as an error.
 # Make sure required directories exist.
 mkdir -p \
     "$XDG_DATA_HOME" \
+    "$XDG_CONFIG_HOME"/FreeFileSync \
     "$XDG_CONFIG_HOME"/gtk-3.0 \
 
 # Copt default files.
 [ -f /config/xdg/config/gtk-3.0/bookmarks ] || cp /defaults/bookmarks /config/xdg/config/gtk-3.0/bookmarks
+[ -f "$XDG_CONFIG_HOME"/FreeFileSync/LastRun.ffs_gui ] || cp /defaults/LastRun.ffs_gui "$XDG_CONFIG_HOME"/FreeFileSync/LastRun.ffs_gui
 
 # Copy the default Firefox profile.
 if [ ! -d /config/ff-profile ]; then
--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -4,4 +4,8 @@
 export HOME=/config
 
 cd /config
-exec /opt/FreeFileSync/Bin/FreeFileSync
+
+# NOTE: Force starting with the `LastRun` profile.  Else, starting with an other
+#       profile changes the title of the window to the path of the profile,
+#       which cause problem to apply the correct Openbox settings.
+exec /opt/FreeFileSync/Bin/FreeFileSync "$XDG_CONFIG_HOME"/FreeFileSync/LastRun.ffs_gui
