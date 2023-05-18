4e0a6aa447500c2b7fc3c5f2a268657f929101a7
Jocelyn Le Sage <jocelyn@le-sage.com>
2023-05-18 18:09:02 -0400

    Updated FreeFileSync to version 12.3.

--- src/freefilesync/build.sh
+++ src/freefilesync/build.sh
@@ -136,13 +136,19 @@ export CXXFLAGS="$CFLAGS"
 export CPPFLAGS="$CFLAGS"
 
 log "Patching FreeFileSync..."
-patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/compilation-fix.patch
-patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/client-credentials.patch
-patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/default-deletion-policy.patch
-patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/default-auto-close.patch
-patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/disable-update.patch
-patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/disable-open-with-default-app.patch
-patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/fix-hang.patch
+PATCHES="
+    compilation-fix.patch
+    client-credentials.patch
+    default-deletion-policy.patch
+    default-auto-close.patch
+    disable-update.patch
+    disable-open-with-default-app.patch
+    fix-hang.patch
+"
+for PATCH in $PATCHES; do
+    log "Applying $PATCH..."
+    patch -p1 -d /tmp/freefilesync < "$SCRIPT_DIR"/"$PATCH"
+done
 
 sed -i "s/GDRIVE_CLIENT_ID_K1\[\] = {}/GDRIVE_CLIENT_ID_K1\[\] = {${GOOGLE_CLIENT_ID_K1}}/" /tmp/freefilesync/FreeFileSync/Source/afs/gdrive.cpp
 sed -i "s/GDRIVE_CLIENT_ID_K2\[\] = {}/GDRIVE_CLIENT_ID_K2\[\] = {${GOOGLE_CLIENT_ID_K2}}/" /tmp/freefilesync/FreeFileSync/Source/afs/gdrive.cpp
