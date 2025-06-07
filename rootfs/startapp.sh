d3e37dfbadd0ada93fab44b75d488b6d3e1a111c
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-07 13:47:13 -0400

    Adjusted selection of the main window. This removes the need to start FreeFileSync with a profile as argument, which prevents starting a comparison automatically when starting.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -5,9 +5,6 @@ export HOME=/config
 
 cd /config
 
-# NOTE: Force starting with the `LastRun` profile.  Else, starting with an other
-#       profile changes the title of the window to the path of the profile,
-#       which cause problem to apply the correct Openbox settings.
-exec /opt/FreeFileSync/Bin/FreeFileSync "$XDG_CONFIG_HOME"/FreeFileSync/LastRun.ffs_gui
+exec /opt/FreeFileSync/Bin/FreeFileSync
 
 # vim:ft=sh:ts=4:sw=4:et:sts=4
